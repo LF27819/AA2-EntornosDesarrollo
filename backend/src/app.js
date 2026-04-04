9962d7a90e2e5faef75ca8a91cb663e390bd3f9c
LF27819 <luciafeliperuiz@gmail.com>
2026-04-04 12:44:33 +0200

    Endpoints consolas funcionando y probados en Postman

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const db = knex({
     useNullAsDefault: true
 });
 
+//videojuegos
 app.get('/videojuegos', async (req, res) => {
     const videojuegos = await db('videojuegos').select('*');
     res.status(200).json(videojuegos);
@@ -56,6 +57,55 @@ app.delete('/videojuegos/:id_videojuego', async (req, res) => {
 
     res.status(204).json({});
 });
+
+//consolas
+
+app.get('/consolas', async (req, res) => {
+    const consolas = await db('consolas').select('*');
+    res.status(200).json(consolas);
+});
+
+app.get('/consolas/:id_consola', async (req, res) => {
+    const consola = await db('consolas')
+        .select('*')
+        .where({ id_consola: req.params.id_consola })
+        .first();
+
+    res.status(200).json(consola);
+});
+
+app.post('/consolas', async (req, res) => {
+    await db('consolas').insert({
+        nombre: req.body.nombre,
+        fabricante: req.body.fabricante,
+        fecha_lanzamiento: req.body.fecha_lanzamiento,
+        precio: req.body.precio,
+        descontinuada: req.body.descontinuada
+    });
+
+    res.status(201).json({});
+});
+
+app.put('/consolas/:id_consola', async (req, res) => {
+    await db('consolas').update({
+        nombre: req.body.nombre,
+        fabricante: req.body.fabricante,
+        fecha_lanzamiento: req.body.fecha_lanzamiento,
+        precio: req.body.precio,
+        descontinuada: req.body.descontinuada
+    }).where({ id_consola: req.params.id_consola });
+
+    res.status(204).json({});
+});
+
+app.delete('/consolas/:id_consola', async (req, res) => {
+    await db('consolas')
+        .del()
+        .where({ id_consola: req.params.id_consola });
+
+    res.status(204).json({});
+});
+
 
 app.listen(8080, () => {
     console.log('El backend ha iniciado en el puerto 8080');
